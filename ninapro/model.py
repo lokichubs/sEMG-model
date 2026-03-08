a412660df2e96bddaa708c41264602ca136f7d4c
lokichubs <loki.p.sriram@gmail.com>
2026-03-08 19:01:05 -0400

    rearranged files and improved file structure

--- ninapro/model.py
+++ ninapro/model.py
@@ -127,7 +127,7 @@ class CNNAttentionImproved(nn.Module):
         window_size=400,
         n_joints=22,
         hidden=256,
-        n_attn=5,
+        n_attn=4,
         n_heads=4,
         dropout=0.1,
     ):
